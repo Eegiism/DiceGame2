918804407a9390fcf834aee3603e647a0901c399
Eegiism <erdenesukh.31gmail.com>
2021-03-11 11:23:11 +0100

    Yes we finished

--- app.js
+++ app.js
@@ -1,59 +1,92 @@
-// First Player = 0, Second Player = 1; 
-var activePlayer = 0;
-
-// Total sum of each player 
-var scores = [0,0];
-
-// Per sound score
-var roundScore = 0;
-
-// Let's prepare the starting programm
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+var scores = [0,0];
+var roundScore;
+var activePlayer;
+var isNewGame;
+
+function initGame() {
+
+	isNewGame = true;
+
+	// First Player = 0, Second Player = 1; 
+	activePlayer = 0;
+
+	// Total sum of each player 
+	scores = [0,0];
+
+	// Per sound score
+	roundScore = 0;
+
+	// Let's prepare the starting programm
+	document.getElementById("current-0").textContent = 0;
+	document.getElementById("current-1").textContent = 0;
+	document.getElementById("score-0").textContent = 0;
+	document.getElementById("score-1").textContent = 0;
+
+	// Show player name
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+
+	document.querySelector('.player-0-panel').classList.remove('winner')
+	document.querySelector('.player-1-panel').classList.remove('winner')
+
+	document.querySelector('.player-0-panel').classList.remove('active')
+	document.querySelector('.player-1-panel').classList.remove('active')
+
+	document.querySelector('.player-0-panel').classList.add('active')
+
+	diceDom.style.display = "none";
+}
+
+//Start New Game
+initGame();
+
 
 // Dice throw EventListener
 document.querySelector(".btn-roll").addEventListener('click', function() {
-	//1-6 random number
-	var diceNumber = Math.floor(Math.random() * 6) + 1;
+	if(isNewGame === true)  {
+		//1-6 random number
+		var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-	// Dice picture show in display
-	diceDom.style.display = "block";
-	diceDom.src = "dice-" + diceNumber + ".png";
+		// Dice picture show in display
+		diceDom.style.display = "block";
+		diceDom.src = "dice-" + diceNumber + ".png";
 
-	// Change player per round score
-	if(diceNumber !== 1 ) {
-		roundScore = roundScore + diceNumber;
-		document.getElementById('current-' + activePlayer).textContent = roundScore;
-	} else {
+		// Change player per round score
+		if(diceNumber !== 1 ) {
+			roundScore = roundScore + diceNumber;
+			document.getElementById('current-' + activePlayer).textContent = roundScore;
+		} else {
 		// Change player turn
 		switchPlayer();	
+		}
+	} else {
+		alert('The Game is Finished. Please press on New Game Button');
 	}
 });
 
 // Press Hold
 document.querySelector(".btn-hold").addEventListener('click', function() {
 	
-	scores[activePlayer] = scores[activePlayer] + roundScore;
+	if(isNewGame) {
+		scores[activePlayer] = scores[activePlayer] + roundScore;
 	
-	//Change display score
-	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-
-	//Check if the player win
-	if(scores[activePlayer] >=10) {
-		document.getElementById('name-' + activePlayer).textContent = "WINNER!!!"
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-
+		//Change display score
+		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+	
+		//Check if the player win
+		if(scores[activePlayer] >=10) {
+			isNewGame = false;
+			document.getElementById('name-' + activePlayer).textContent = "WINNER!!!"
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	
+		} else {
+			switchPlayer();
+		}
 	} else {
-		switchPlayer();
+		alert('The Game is Finished. Please press on New Game Button');
 	}
-
-
 })
 
 // Switch to next player
@@ -68,3 +101,6 @@ function switchPlayer() {
 	
 	diceDom.style.display = "none";
 }
+
+// Starting new game event listener
+document.querySelector('.btn-new').addEventListener('click', initGame)
